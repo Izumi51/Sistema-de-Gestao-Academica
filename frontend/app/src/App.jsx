c810d43cebadb9818fc232f60f908c03659649c6
Nathaly <nathaly.jp03@gmail.com>
2026-01-28 21:59:07 -0300

    config: router-dom setup

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from "./pages/home/home";
 
 function App(){
     return(
-        <h1>teste</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='/login' element="Login Page"></Route>
+                <Route path='/*' element="404 temporary"></Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
