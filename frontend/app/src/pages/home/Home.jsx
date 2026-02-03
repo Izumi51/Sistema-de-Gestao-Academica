eb12d1c3aa53a009538e8e56d6e14520c22e4e5f
Nathaly <nathaly.jp03@gmail.com>
2026-02-02 21:31:39 -0300

    feat: creating hero section

--- frontend/app/src/pages/home/Home.jsx
+++ frontend/app/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import styles from './Home.module.css'
 import Logo from '../../assets/logo.png'
+import HeroImg from '../../assets/heroSectionImg.png'
 import { BlueButton } from '../../components/BlueButton/BlueButton'
 
 export function Home(){
@@ -19,6 +20,14 @@ export function Home(){
                     <BlueButton to="/login" text="Acessar Portal"/>
                 </nav>
             </header>
+            <section className= {styles.heroSection}>
+                <div>
+                    <h1>Organize sua instituição com <span>IA</span> que automatiza rotinas e melhora a comunicação.</h1>
+                    <h2>Atendimento automatizado, gestão de turmas, avaliações, frequência e painéis analíticos — tudo em um só sistema.</h2>
+                    <BlueButton to="/login" text="Comece Agora!"/>
+                </div>
+                <img src={HeroImg} alt="imagem temporária" />
+            </section>
         </>
     )
 }
